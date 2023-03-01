3bad765ee3b1a02ca60c6e7b00fffb3b16ff0b03
nkumar007 <naveen.mbuff98@gmail.com>
2023-03-01 20:08:14 +0530

    building for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: {
+    enabled: true,
+    content: ["./src/index.html"],
+  },
+  mode: "jit",
+  purge: ["./src/index.html"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
